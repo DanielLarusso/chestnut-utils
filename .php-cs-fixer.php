4e34f772df940b18a5bba166d11f72c0d9d6098c
Daniel Pogodda <daniel.pogodda@transfermarkt.de>
2023-02-03 12:27:14 +0100

    add php-cs-fixer configuration

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.14.3|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        // ->exclude('folder-to-exclude') // if you want to exclude some folders, you can do it like this!
+        ->in(__DIR__)
+    )
+;
